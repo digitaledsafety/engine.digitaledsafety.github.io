0462a6c4595933eb9161022f7d78b8603dc00fdd
brettfxio <105930054+brettfxio@users.noreply.github.com>
2025-11-26 21:14:46 -0700

    Revert "Revert "feat: Add standard Blockly color blocks""

--- script.js
+++ script.js
@@ -872,10 +872,6 @@ var toolbox = {
                 {
                     kind: 'block',
                     type: 'procedural_texture'
-                },
-                {
-                    kind: 'block',
-                    type: 'colour_picker'
                 }
             ]
         },
@@ -930,6 +926,29 @@ var toolbox = {
                     type: 'set_texture_from_asset',
                 }
             ]
+        },
+        {
+            kind: 'category',
+            name: 'Colour',
+            categorystyle: 'colour_category',
+            contents: [
+                {
+                    kind: 'block',
+                    type: 'colour_picker',
+                },
+                {
+                    kind: 'block',
+                    type: 'colour_random',
+                },
+                {
+                    kind: 'block',
+                    type: 'colour_rgb',
+                },
+                {
+                    kind: 'block',
+                    type: 'colour_blend',
+                },
+            ],
         },
         {
             kind: 'category',
@@ -4273,23 +4292,7 @@ Blockly.Themes.DigitalEducationSafety = Blockly.Theme.defineTheme('digital-educa
                 "colour": 180,
                 "tooltip": "Selects a procedural texture for the background.",
                 "helpUrl": ""
-            },
-            {
-                "type": "colour_picker",
-                "message0": "%1",
-                "args0": [
-                    {
-                        "type": "field_colour",
-                        "name": "COLOUR",
-                        "colour": "#ff0000"
-                    }
-                ],
-                "output": "String",
-                "colour": "%{BKY_COLOUR_HUE}",
-                "tooltip": "Select a color.",
-                "helpUrl": ""
             }
-            
         ]);
 
         {
